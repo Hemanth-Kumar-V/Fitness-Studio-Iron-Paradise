f30bd19a172e2459e39e9249dfa7dd8782f3da86
Hemanth Kumar V <hemanth08kumarv@gmail.com>
2023-12-16 23:51:26 +0530

    Added Contact Us Section with the database.

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$mobile = $_POST['mobile'];
+	$message = $_POST['message'];
+	
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','contactus');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into contactus(name, email, mobile, message) values(?, ?, ?, ?)");
+		$stmt->bind_param("ssis", $name, $email, $mobile, $message);
+		$execval = $stmt->execute();
+		echo $execval;
+		// echo "Registration successfully...";
+        if (!$execval) {
+            echo "Error: " . $stmt->error;
+         } else {
+             echo "Registration successfully...";
+         }
+
+		$stmt->close();
+		$conn->close();
+	}
+?>
